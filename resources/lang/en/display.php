541451e815672601b681fdfdccab50cfa3c88c42
ZSumiyabazar0303 <zorigtsumyabazar@gmail.com>
2022-09-07 17:24:47 +0800

    ss

---
+++ resources/lang/en/display.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Display Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'manage' => 'Үйлдэл',
+    'filter' => 'Шүүх',
+    'add_building' => 'Барилга нэмэх',
+    'building_code' => 'Барилгын регистр',
+    'building_name' => 'Барилгын нэр',
+    'building_parent_id' => 'Харъяа Барилга',
+    'building_address' => 'Барилгын хаяг',
+    'building_phone' => 'Барилгын утасны дугаар',
+    
+
+];
